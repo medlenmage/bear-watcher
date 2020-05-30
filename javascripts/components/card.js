11d2055e25c283f360f906b98fc018f74cf252b5
Joshua Medlen <joshua246@gmail.com>
2020-05-30 12:12:35 -0500

    finished card building function and data pushing function

--- javascripts/components/card.js
+++ javascripts/components/card.js
@@ -1,18 +1,33 @@
 import utils from '../helpers/utils.js'
+import bearData from '../helpers/data/bearData.js';
+
+const addToBears = (bear) => {
+bearData.addBears(bear);
+createCards();
+}
 
 const createCards = () => {
+
+  const getBears = bearData.getBear()
+
   let domString = '';
-  domString += `
-  <div class="card" style="width: 18rem;">
-    <img class="card-img-top" src="..." alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+
+  for (let i = 0; i < getBears.length; i++) {
+    domString += `
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="${getBears[i].img}" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">${getBears[i].type}</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
     </div>
-  </div>
-  `
+    `
+  }
+
   utils.printToDom('#cards-bear', domString)
 }
 
-export default { createCards }
+// ${getBears[i].type}
+
+export default { createCards, addToBears }
--- javascripts/components/form.js
+++ javascripts/components/form.js
@@ -1,4 +1,11 @@
 import utils from '../helpers/utils.js'
+import pushBear from '../helpers/data/bearData.js'
+import card from '../components/card.js'
+
+const addBear = (e) => {
+  e.preventDefault();
+  card.addToBears();
+}
 
 const createForm = () => {
 
@@ -8,16 +15,16 @@ const createForm = () => {
   <form>
     <div class="form-group">
       <label for="exampleInputEmail1">Bears Enter Here</label>
-      <input type="email" class="form-control form-bar" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Your Bear">
+      <input class="form-control form-bar" id="bearType" aria-describedby="emailHelp" placeholder="Your Bear">
       <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-      <input type="email" class="form-control form-bar" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Bear Url Here">
+      <input class="form-control form-bar" id="bearImg" aria-describedby="emailHelp" placeholder="Bear Url Here">
     </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <button type="submit" class="btn btn-primary" id="add-to-bear">Submit</button>
   </form>
   `;
 
   utils.printToDom('#form-bear', domString)
-  
+  $('body').on('click', '#add-to-bear', addBear)
 }
 
 export default { createForm }
